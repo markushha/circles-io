a3e9f1aa9db5dc40ba5e9b0859418dd4d0a908d1
Mark Inger <mark@starfund.app>
2023-05-20 16:01:06 +0600

    added transtion

--- src/app/components/ui/Navbar.tsx
+++ src/app/components/ui/Navbar.tsx
@@ -11,7 +11,7 @@ function Navbar() {
   }
 
   return (
-    <div className="w-full h-16 flex items-center p-8 shadow-lg text-slate-950 font-semibold dark:text-slate-200 bg-slate-300 dark:bg-slate-400">
+    <div className="w-full h-16 flex items-center p-8 shadow-lg text-slate-950 font-semibold dark:text-slate-200 bg-slate-100 dark:bg-slate-800 shadow-xl">
       <div className="w-[70%] h-[100vh] mx-auto flex items-center justify-center">
         <div className="mr-auto"></div>
         <div className="ml-auto">
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,8 +3,8 @@ import Layout from "@/app/components/layout/Layout";
 export default function Home() {
   return (
     <Layout
-      seoTitle="Mark Inger"
-      description="Mark Inger - Front-End Engineer from Almaty, Kazakhstan. I'm here to provide my clients with the best service and build most powerful websites"
+      seoTitle="Shadcn Time"
+      description="Time managment app"
     >
       
     </Layout>
